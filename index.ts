0a8261c42395891623618c54a4e30d6680a42fae
wow-woo <fwpass@outlook.com>
2020-09-15 15:08:38 +0900

    weighted quick-uion

--- index.ts
+++ index.ts
@@ -5,13 +5,17 @@ type targets = {
 
 class QuickUnionUF {
   arr: number[];
+  sz: number[];
+
   constructor(n: number) {
     this.arr = Array(n);
+    this.sz = Array(n).fill(1);
 
     for (let i = 0; i < n; i++) {
       this.arr[i] = i;
     }
-    console.log("init", this.arr);
+    console.log("init-arr", this.arr);
+    console.log("init-sz", this.sz);
   }
 
   root(a: number): number {
@@ -30,9 +34,19 @@ class QuickUnionUF {
   union(p: number, q: number): void {
     const i = this.root(p);
     const j = this.root(q);
+    if (i === j) return;
 
-    this.arr[i] = j;
+    if (this.sz[i] < this.sz[j]) {
+      this.arr[i] = j;
 
+      this.sz[j] += this.sz[i];
+    } else {
+      this.arr[j] = i;
+
+      this.sz[i] += this.sz[j];
+    }
+
+    console.log("weight", this.sz);
     return console.log(this.arr);
   }
 }
